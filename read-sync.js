9f6ec7d5e45bc73a8bde72554a163a4727b0e318
islamemon59 <islamemon59@gmail.com>
2025-11-22 09:33:21 +0600

    read async file and sync file with node fs

---
+++ read-sync.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+console.log("Start Reading");
+
+try{
+console.log("File Content");
+const data = fs.readFileSync("./data/diary.txt", "utf-8")
+console.log(data);
+}catch(error){
+    console.log(error.message);
+}
+
+console.log("Finished");
